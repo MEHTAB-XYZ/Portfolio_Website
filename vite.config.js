e9aeeb513c6050660894ece4deca6ed0f833961f
MEHTAB <mehtabarifahad@gmail.com>
2025-11-26 20:31:33 +0530

    Deploy build to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Portfolio_Website/",   // <-- FIXED
-})
+  base: "/Portfolio_Website/",
+  build: {
+    outDir: "docs",   // 👈 THIS is required
+  },
+});
